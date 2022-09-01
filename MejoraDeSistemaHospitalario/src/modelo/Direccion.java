9c4ab441ff81b814fd1608a41a68885e85e2c9c8
YeffreyJCB <112532663+YeffreyJCB@users.noreply.github.com>
2022-08-31 21:14:35 -0500

    Add files via upload

---
+++ MejoraDeSistemaHospitalario/src/modelo/Direccion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Jair Javier Fernandez Almeida 
+ *         Yeffrey Jair Carbajal Barcia 
+ */
+public class Direccion {
+    private String ciudad;
+    private String calle;
+    private int numero;
+    
+    public Direccion(String ciudad, String calle, int numero){
+        
+    }
+    
+}
---
+++ MejoraDeSistemaHospitalario/src/principal/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+
+import gui.InterfazGrafica;
+import modelo.Paciente;
+import modelo.Persona;
+
+/**
+ *
+ * @author windos
+ */
+public class Main {
+    public static void main(String args[]){
+       InterfazGrafica inter = new InterfazGrafica("Programa El paciente feliz");
+       inter.setVisible(true);
+                
+       Persona paciente1 = new Paciente();
+       
+        
+    }  
+    
+}
